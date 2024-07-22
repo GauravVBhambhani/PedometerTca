f16e819b74e896b20823708fb325b85bd35d50f5
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-07-21 20:56:13 -0400

    Initial Commit

---
+++ PedometerTca/PedometerTcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerTcaApp.swift
+//  PedometerTca
+//
+//  Created by Gaurav Bhambhani on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PedometerTcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
